0549c37f7b39b2303bd15645c79e253ea3b791f9
Diego Nunes <nunesdiegond@gmail.com>
2022-11-09 01:31:41 -0300

    commit 008 - cominicação com o banco de dados funcionando, criados pasta de repositórios de migrations

---
+++ Repositorio/ContatoRepositorio.cs
@@ -0,0 +1,12 @@
+﻿using CadastroContatos.Models;
+
+namespace CadastroContatos.Repositorio
+{
+    public class ContatoRepositorio : IContatoRepositorio
+    {
+        public ContatoModel Adicionar(ContatoModel contato)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
